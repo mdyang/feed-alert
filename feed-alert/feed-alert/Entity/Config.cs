1a469c03cb572fe0bae0236ee701819ede896eb4
mdyang <conanholmes001@gmail.com>
2013-12-24 23:57:15 +0800

    finish backlog work item - Feed diff, when new feed comes, decide which to notify

---
+++ feed-alert/feed-alert/Entity/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace feed_alert.Entity
+{
+    using System.Runtime.Serialization;
+
+    [DataContractAttribute]
+    class Config
+    {
+        [DataMember]
+        public int UpdateInterval { get; set; }
+
+    }
+}
