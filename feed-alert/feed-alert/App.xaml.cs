126e0e73404dcb0b64da28c809912fc93a1f078c
Mengdong Yang <conanholmes001@gmail.com>
2013-12-25 17:22:49 +0800

    single process instance

--- feed-alert/feed-alert/App.xaml.cs
+++ feed-alert/feed-alert/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -29,23 +30,39 @@ namespace feed_alert
             }
         }
 
+        private static bool running = false;
+
+        private static bool mutexCreated = false;
+        private static Mutex mutex = new Mutex(true, @"26041D0E_E96A_436D_B1C0_E6C6BDA3CA80", out mutexCreated);
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            // ensure single instance
+            if (!mutexCreated)
+            {
+                Current.Shutdown();
+                return;
+            }
+
             appScheduler = TaskScheduler.FromCurrentSynchronizationContext();
             TrayIconUtility.TrayIcon.Visible = true;
             PersistenceFacade.LoadFeedSources();
             PersistenceFacade.LoadFeedSourceStateStore();
             Updater.Start();
             Notifier.Start();
+            running = true;
         }
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
-            TrayIconUtility.TrayIcon.Visible = false;
-            Updater.Stop();
-            Notifier.Stop();
-            PersistenceFacade.SaveFeedSources();
-            PersistenceFacade.SaveFeedSourceState();
+            if (running)
+            {
+                TrayIconUtility.TrayIcon.Visible = false;
+                Updater.Stop();
+                Notifier.Stop();
+                PersistenceFacade.SaveFeedSources();
+                PersistenceFacade.SaveFeedSourceState();
+            }
         }
     }
 }
